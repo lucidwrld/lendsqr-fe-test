70952742a119a3a3a79c27fb4ac7cff66496bc1f
lucidwrld <kabiawubenedict@gmail.com>
2023-06-05 12:26:36 +0100

    Updated

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -9,21 +9,7 @@ const Dashboard: React.FC = () => {
         <div className='title'>
           <h1>DashBoard</h1>
         </div>
-        <div className='headings'>
-          <div className="card">
-            <div className="card-top">
-              <h1>Streamline operations and enhance customer service</h1>
-            </div>
-            <div className="card-footer"></div>
-          </div>
-          <div className="card"><div className="card-top">
-            <h1>Unlocking the potential of our staff</h1>
-          </div>
-            <div className="card-footer"></div></div>
-          <div className="card"><div className="card-top">
-            <h1>Empowering our staff with powerful financial tools</h1></div>
-            <div className="card-footer"></div></div>
-        </div>
+
         <div className="content">
           <h1>Welcome!...</h1>
         </div>
